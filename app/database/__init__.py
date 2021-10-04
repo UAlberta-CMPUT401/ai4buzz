1bef3d7578ae8b36336b30ad1918799e29ad3239
neilZon <neilcviloria@gmail.com>
2021-10-04 09:58:45 -0600

    setup db connection

---
+++ app/database/__init__.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://ohhykmmy:kC5gr49OfXLn1Kkufk_y1CtHJUAhTPwM@kashin.db.elephantsql.com/ohhykmmy"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
