adbb98f9a8169b1d0cfbb7ca37612598af91b51f
neilZon <neilcviloria@gmail.com>
2021-10-13 17:09:52 -0600

    move ml models

--- api/database/__init__.py
+++ api/database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://ohhykmmy:kC5gr49OfXLn1Kkufk_y1CtHJUAhTPwM@kashin.db.elephantsql.com/ohhykmmy"
+SQLALCHEMY_DATABASE_URL = "postgresql://ohhykmmy:kC5gr49OfXLn1Kkufk_y1CtHJUAhTPwM@kashin.db.elephantsql.com/ohhykmmy"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
