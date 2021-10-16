f4f5382d7401e781019ab84bbd034d108c84c44a
neilZon <neilcviloria@gmail.com>
2021-10-16 12:35:35 -0600

    revert to using 3rd party db

--- api/database/__init__.py
+++ api/database/__init__.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://ubuntu:ubuntu@[2605:fd00:4:1001:f816:3eff:fe53:254a]:5432/postgres"
-
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://ubuntu:ubuntu@2d7a6.yeg.rac.sh/postgres" # TODO: ipv6 psycopg2 docker build error
+SQLALCHEMY_DATABASE_URL = "postgresql://ohhykmmy:kC5gr49OfXLn1Kkufk_y1CtHJUAhTPwM@kashin.db.elephantsql.com/ohhykmmy"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
