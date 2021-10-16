f31e333d0e99fd511647031e86737c5bab402b19
neilZon <neilcviloria@gmail.com>
2021-10-16 12:35:35 -0600

    revert to using 3rd party db

--- api/database/models.py
+++ api/database/models.py
@@ -7,5 +7,4 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    name = Column(String)
-    password = Column(String)
+    hashed_password = Column(String)
