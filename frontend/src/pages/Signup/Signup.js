5eb0beb5e9e0d93c19fdcfc8b96887c0a2969418
neilZon <neilcviloria@gmail.com>
2021-11-08 11:07:09 -0700

    frontend auth form

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
+
+import { signup } from '../../services/auth';
+import SigninForm from '../../components/SigninForm/SigninForm';
 
 const Signup = () => {
-  return <div>signup</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async () => {
+    const res = await signup(email, password);
+    Cookies.set('access_token', res.data.access_token, { path: '/' });
+  };
+
+  return (
+    <div>
+      <SigninForm
+        authType="Signup"
+        onEmailChange={setEmail}
+        onPasswordChange={setPassword}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  );
 };
 
 export default Signup;
