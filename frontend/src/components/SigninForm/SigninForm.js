4f55003c183475cf0208791222cdb3b7601d361f
neilZon <neilcviloria@gmail.com>
2021-11-11 13:50:36 -0700

    sign in form styling

--- frontend/src/components/SigninForm/SigninForm.js
+++ frontend/src/components/SigninForm/SigninForm.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './SigninForm.module.css';
 
 const SigninForm = ({
   authType,
@@ -12,16 +15,34 @@ const SigninForm = ({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="email"
-        onChange={(e) => onEmailChange(e.target.value)}
-      />
-      <input
-        placeholder="password"
-        onChange={(e) => onPasswordChange(e.target.value)}
-      />
-      <button>{authType}</button>
+    <form onSubmit={handleSubmit} className={styles.signinForm}>
+      <div>
+        <label className={styles.labelName}>Email</label>
+        <input
+          placeholder="email"
+          className={styles.signinInput}
+          type="email"
+          onChange={(e) => onEmailChange(e.target.value)}
+        />
+      </div>
+      <div>
+        <label className={styles.labelName}>Password</label>
+        <input
+          placeholder="password"
+          className={styles.signinInput}
+          type="password"
+          onChange={(e) => onPasswordChange(e.target.value)}
+        />
+      </div>
+      <div className="buttonGradient">
+        <button className={styles.submitButton}>{authType}</button>
+      </div>
+      <Link
+        to={`/${authType === 'Login' ? 'signup' : 'login'}`}
+        className={styles.switchAuth}
+      >
+        {`${authType === 'Login' ? 'Signup' : 'Login'}`}
+      </Link>
     </form>
   );
 };
