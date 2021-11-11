28f8e8cd441ce895e606030ff9548c1392eba89f
neilZon <neilcviloria@gmail.com>
2021-11-11 15:05:42 -0700

    auth errors

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
+import { useHistory } from 'react-router';
 
 import { login } from '../../services/auth';
 import SigninForm from '../../components/SigninForm/SigninForm';
+import styles from './Login.module.css';
 
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [error, setError] = useState();
+  const history = useHistory();
 
   const handleSubmit = async () => {
-    const res = await login(email, password);
-    console.log(res);
-    Cookies.set('access_token', res.data.access_token, { path: '/' });
+    try {
+      const res = await login(email, password);
+      Cookies.set('access_token', res.data.access_token, { path: '/' });
+      history.push('/analyze-image');
+    } catch (e) {
+      setError(e.response.data);
+    }
   };
 
   return (
-    <div>
+    <div className={styles.loginPage}>
+      <h1 className={styles.title}>AI4Buzz</h1>
       <SigninForm
         authType="Login"
         onSubmit={handleSubmit}
         onEmailChange={setEmail}
         onPasswordChange={setPassword}
+        error={error}
       />
     </div>
   );
