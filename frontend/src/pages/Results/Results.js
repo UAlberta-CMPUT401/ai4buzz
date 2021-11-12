0e17f4e150d327e8f5131815e155412f9eca75a0
neilZon <neilcviloria@gmail.com>
2021-11-11 21:07:06 -0700

    display results

--- frontend/src/pages/Results/Results.js
+++ frontend/src/pages/Results/Results.js
@@ -1,27 +1,67 @@
 import React from 'react';
+import ImageClassificationResults from '../../components/ImageClassificationResults/ImageClassificationResults';
+import ObjectDetectionResults from '../../components/ObjectDetectionResults/ObjectDetectionResults';
+import SentimentClassification from '../../components/SentimentClassification/SentimentClassification';
 import styles from './Results.module.css';
 
 const Results = ({ data, images }) => {
-  console.log(data, images);
-
   return (
     <div className={styles.resultsPage}>
-      <h1 className={styles.resultTitle}>Image Results</h1>
-      {images.map((image, idx) => {
-        return (
-          <div key={idx}>
-            <div>{image.name}</div>
-            <img
-              className={styles.image}
-              src={URL.createObjectURL(image)}
-              alt="results"
-            />
-            <div>{JSON.stringify(data)}</div>
-          </div>
-        );
-      })}
-      <div>
-        <div className={styles.results}></div>
+      <h1 className={styles.resultTitle}>Analysis Results</h1>
+      <div className={styles.resultList}>
+        {images.map((image, idx) => {
+          return (
+            <div key={idx}>
+              <h4>{image.name}</h4>
+              <div className={styles.imageContainer}>
+                <img
+                  className={styles.image}
+                  src={URL.createObjectURL(image)}
+                  alt="results"
+                />
+              </div>
+              <div>
+                <div className={styles.gradient}>
+                  <div className={styles.analysisHeader}>Color Analysis</div>
+                </div>
+                <div>{JSON.stringify(data.color_scheme_analysis)}</div>
+                <div className={styles.gradient}>
+                  <div className={styles.analysisHeader}>Object Detection</div>
+                </div>
+                <div>
+                  <ObjectDetectionResults results={data.object_detection} />
+                </div>
+                <div className={styles.gradient}>
+                  <div className={styles.analysisHeader}>
+                    Image Classification
+                  </div>
+                </div>
+                <div>
+                  <ImageClassificationResults
+                    results={data.image_classification}
+                  />
+                </div>
+                <div className={styles.gradient}>
+                  <div className={styles.analysisHeader}>
+                    Sentiment Classification
+                  </div>
+                </div>
+                <div>
+                  <SentimentClassification
+                    results={data.sentiment_analysis.image_1.degrees}
+                  />
+                </div>
+                <div className={styles.gradient}>
+                  <div className={styles.analysisHeader}>Text Recognition</div>
+                </div>
+                <div>
+                  Extracted:{' '}
+                  <div className="value">{data.text_recognition}</div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
