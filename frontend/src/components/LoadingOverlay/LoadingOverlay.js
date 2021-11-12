ac1894f86a941cd14e12c43c0b28087c83961273
neilZon <neilcviloria@gmail.com>
2021-11-11 20:15:53 -0700

    loading overlay

--- frontend/src/components/LoadingOverlay/LoadingOverlay.js
+++ frontend/src/components/LoadingOverlay/LoadingOverlay.js
@@ -6,9 +6,12 @@ import styles from './LoadingOverlay.module.css';
 const LoadingOverlay = () => {
   return (
     <div className={styles.loadingOverlay}>
-      <object type="image/svg+xml" data={Spinner}>
-        svg-animation
-      </object>
+      <div>
+        <object type="image/svg+xml" data={Spinner}>
+          svg-animation
+        </object>
+        <div style={{ textAlign: 'center' }}>This might take a bit...</div>
+      </div>
     </div>
   );
 };
