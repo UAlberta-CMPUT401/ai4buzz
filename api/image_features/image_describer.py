c6954fd156dfb91367487d18c4e2a872296c7cea
aschafer1 <aschafer@ualberta.ca>
2021-11-13 18:00:07 -0700

    image describer with face detection

--- api/image_features/image_describer.py
+++ api/image_features/image_describer.py
@@ -11,10 +11,13 @@ from api.image_features.sentiment_analysis.sentiment_analyzer import SentimentAn
 from api.image_features.image_classification.image_classifiier import ImageClassifier
 from api.image_features.tf_hub_client import TFHubClient
 from api.image_features.text_recognition.text_recognizer import TextRecognizer
+from api.image_features.facial_analysis.facial_detector import FaceDetector
+import base64
+from io import BytesIO
 
 
 class ImageDescriber():
-
+    """Extracts features for images."""
     def get_features_by_image(self, images):
         """ perform analysis and data extraction on image 
 
@@ -22,25 +25,28 @@ class ImageDescriber():
         :return: dict containing formatted analysis data
         """
         report_generator_ = ReportGenerator()
+        tf_hub_client = TFHubClient()
+        colorSchemeAnalyzer = ColorSchemeAnalyzer()
+        object_detector = ObjectDetector(tf_hub_client)
+        image_classifier = ImageClassifier(tf_hub_client)
+        sentiment_analyzer = SentimentAnalyzer(batch_size=1)
+        text_recognizer = TextRecognizer()
+        face_detector = FaceDetector()
+
         feature_analysis_results = {}
-
         for idx, image in enumerate(images):
-            colorSchemeAnalyzer = ColorSchemeAnalyzer()
             color_scheme_analysis = colorSchemeAnalyzer.get_descriptions(image)
-
-            object_detector = ObjectDetector(TFHubClient())
+           
             object_detections_descriptions = object_detector.get_descriptions(image)
             object_detection_report = report_generator_.generate_report(object_detections_descriptions)
-
-            image_classifier = ImageClassifier(TFHubClient())
+           
             image_classification_descreptions = image_classifier.get_descriptions(image)
             image_classification_report = report_generator_.generate_report(image_classification_descreptions)
-
-            text_recognizer = TextRecognizer()
-            text = text_recognizer.get_descriptions(image)
-
-            sentiment_analyzer = SentimentAnalyzer(batch_size=1)
+          
+       
             sentiment_analysis = sentiment_analyzer.get_descriptions([image])
+            text = text_recognizer.get_descriptions(image)
+            face_analysis = face_detector.get_descriptions(image)
 
             feature_analysis_results["image_" + str(idx + 1)] = {
                 "color_scheme_analysis": color_scheme_analysis,
@@ -48,18 +54,27 @@ class ImageDescriber():
                 "sentiment_analysis": sentiment_analysis,
                 "image_classification": image_classification_report,
                 "text_recognition": text,
+                "face_analysis": face_analysis,
             }
-
 
         collage_generator = CollageGenerator()
         collage = collage_generator.generate(images)
 
+        # convert to base64
+        buffer = BytesIO()
+        collage_rgb = collage.convert('RGB')
+        collage_rgb.save(buffer, format="JPEG")
+        collage_image_string = base64.b64encode(buffer.getvalue())
+
         # TODO: convert to base64 encoded string
-        # feature_analysis_results['collage'] = collage_base64_string
+        feature_analysis_results['collage'] = collage_base64_string
 
         dendrogram_generator = DendrogramGenerator()
         dendrogram = dendrogram_generator.generate(feature_analysis_results) 
         # TODO: convert to base64 encoded string
         # feature_analysis_results['dendrogram'] = dendrogram_base64_string
 
-        return feature_analysis_results
+        return {
+            "feature_analysis_results": feature_analysis_results,
+            "collage_image_string": collage_image_string
+        }
