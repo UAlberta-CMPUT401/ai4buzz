14261dbcecc7dc718775584b0b5128e4e6295202
aschafer1 <aschafer@ualberta.ca>
2021-11-19 10:51:45 -0700

    made report and return class variables

--- api/image_features/dendrogram_generator/dendrogram_generator.py
+++ api/image_features/dendrogram_generator/dendrogram_generator.py
@@ -5,7 +5,8 @@ import json
 
 class DendrogramGenerator: 
     def __init__(self):
-        pass
+        self.formatted_data = None
+        self.data = None
 
     def generate(self, report):
         """ 
@@ -14,50 +15,50 @@ class DendrogramGenerator:
         :param report: Dictionary of the feature analysis
         :returns type: Dendrogram
         """
-        data = report
-        num_images = len(data['feature_analysis_results'])
-        total_data = np.zeros((num_images,10))
+        self.data = report
+        num_images = len(self.data['feature_analysis_results'])
+        self.formatted_data = np.zeros((num_images,10))
         for i in range(num_images):
   
-            total_data[i][0] = (data['feature_analysis_results'][i]['color_scheme_analysis']['count'])
-            total_data[i][1] = (data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['red'])
-            total_data[i][2] = (data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['green'])
-            total_data[i][3] = (data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['blue'])
-            total_data[i][4] = (data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][0])
-            total_data[i][5] = (data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][1])
-            total_data[i][6] = (data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][2])
+            self.formatted_data[i][0] = (self.data['feature_analysis_results'][i]['color_scheme_analysis']['count'])
+            self.formatted_data[i][1] = (self.data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['red'])
+            self.formatted_data[i][2] = (self.data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['green'])
+            self.formatted_data[i][3] = (self.data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['blue'])
+            self.formatted_data[i][4] = (self.data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][0])
+            self.formatted_data[i][5] = (self.data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][1])
+            self.formatted_data[i][6] = (self.data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][2])
 
-            if data['feature_analysis_results'][i]['text_recognition'] == "":
-                total_data[i][7] = 0
+            if self.data['feature_analysis_results'][i]['text_recognition'] == "":
+                self.formatted_data[i][7] = 0
             else:
-                total_data[i][7] = 1
+                self.formatted_data[i][7] = 1
 
-            object_keys = data['feature_analysis_results'][i]["object_detection"].keys()
+            object_keys = self.data['feature_analysis_results'][i]["object_detection"].keys()
             #print(object_keys)
             object_count = 0
             for key in object_keys:
                 if key != 'processes_bounding_boxes_image_as_base64_string':
-                    object_count = object_count + data['feature_analysis_results'][i]["object_detection"][key]["freq"] 
-            total_data[i][8] = object_count
+                    object_count = object_count + self.data['feature_analysis_results'][i]["object_detection"][key]["freq"] 
+            self.formatted_data[i][8] = object_count
 
-            total_data[i][9] = data['feature_analysis_results'][i]["face_analysis"]["count"]
+            self.formatted_data[i][9] = self.data['feature_analysis_results'][i]["face_analysis"]["count"]
 
-        #print(total_data)
-        #print(total_data.shape)
+        print(self.formatted_data)
 
-        for i in range(total_data.shape[1]):
-            if total_data[:,i].max() != 0 :
+        for i in range(self.formatted_data.shape[1]):
+            if self.formatted_data[:,i].max() != 0 :
 
-                total_data[:,i] = total_data[:,i] / total_data[:,i].max()
+                self.formatted_data[:,i] = self.formatted_data[:,i] / self.formatted_data[:,i].max()
 
-        #print(total_data)
+        print(self.formatted_data)
         #try cosine
-        temp = hierarchy.linkage(total_data, 'single')
+        temp = hierarchy.linkage(self.formatted_data, 'single')
         plt.figure()
   
         dn = hierarchy.dendrogram(temp)
         plt.savefig('api/image_features/dendrogram_generator/dendrogram.png')
 
+    #def add_colours(self)
 
 if __name__ == '__main__':
    
