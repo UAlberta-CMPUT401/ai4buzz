4f7aa179bfe68a930ab62a5f1bfba460e927f76c
aschafer1 <aschafer@ualberta.ca>
2021-11-18 21:51:09 -0700

    basic dendrogram generator which saves a png to the folder

--- api/image_features/dendrogram_generator/dendrogram_generator.py
+++ api/image_features/dendrogram_generator/dendrogram_generator.py
@@ -1,3 +1,8 @@
+import numpy as np
+from scipy.cluster import hierarchy
+import matplotlib.pyplot as plt
+import json
+
 class DendrogramGenerator: 
     def __init__(self):
         pass
@@ -9,4 +14,56 @@ class DendrogramGenerator:
         :param report: Dictionary of the feature analysis
         :returns type: Dendrogram
         """
-        pass
+        data = report
+        num_images = len(data['feature_analysis_results'])
+        total_data = np.zeros((num_images,9))
+        for i in range(num_images):
+  
+            total_data[i][0] = (data['feature_analysis_results'][i]['color_scheme_analysis']['count'])
+            total_data[i][1] = (data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['red'])
+            total_data[i][2] = (data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['green'])
+            total_data[i][3] = (data['feature_analysis_results'][i]['color_scheme_analysis']['colors'][0]['blue'])
+            total_data[i][4] = (data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][0])
+            total_data[i][5] = (data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][1])
+            total_data[i][6] = (data['feature_analysis_results'][i]['sentiment_analysis']['sentiment_array[neg,neu,pos]'][2])
+
+            if data['feature_analysis_results'][i]['text_recognition'] == "":
+                total_data[i][7] = 0
+            else:
+                total_data[i][7] = 1
+
+            object_keys = data['feature_analysis_results'][i]["object_detection"].keys()
+            #print(object_keys)
+            object_count = 0
+            for key in object_keys:
+                if key != 'processes_bounding_boxes_image_as_base64_string':
+                    object_count = object_count + data['feature_analysis_results'][i]["object_detection"][key]["freq"] 
+            total_data[i][8] = object_count
+
+
+        #print(total_data)
+        #print(total_data.shape)
+
+        for i in range(total_data.shape[1]):
+            if total_data[:,i].max() != 0 :
+
+                total_data[:,i] = total_data[:,i] / total_data[:,i].max()
+
+        print(total_data)
+
+        temp = hierarchy.linkage(total_data, 'single')
+        plt.figure()
+  
+        dn = hierarchy.dendrogram(temp)
+        plt.savefig('api/image_features/dendrogram_generator/dendrogram.png')
+
+
+if __name__ == '__main__':
+   
+    with open('api/image_features/dendrogram_generator/7response.json') as f:
+        data = json.load(f)
+
+    #print(data['0'])
+    gen = DendrogramGenerator()
+    gen.generate(data)
+    
