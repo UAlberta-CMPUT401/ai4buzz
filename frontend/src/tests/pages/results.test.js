e8f8a733a227a8245140e67e5966574538c9b198
Neilzon Viloria <neilcviloria@gmail.com>
2021-11-23 12:47:02 -0700

    fix failing tests

--- frontend/src/tests/pages/results.test.js
+++ frontend/src/tests/pages/results.test.js
@@ -4,8 +4,9 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 
 const mockData = {
-  feature_analysis_results: {
-    image_1: {
+  feature_analysis_results: [
+    {
+      image_1: "image_id",
       color_scheme_analysis: {
         count: 5,
         colors: [
@@ -39,8 +40,31 @@ const mockData = {
         television: 0.014548316597938538,
       },
       text_recognition: '',
+      face_analysis: {
+        analysis: [
+          {age: 22,
+dominant_emotion: "happy",
+dominant_race: "white",
+emotion: {angry: 5.888953067055257e-8,
+  disgust: 9.406594389845943e-11,
+  fear: 5.528744358280318e-7,
+  happy: 99.98632669611851,
+  neutral: 0.013652960306425654,
+  sad: 5.4525238929900076e-8,
+  surprise: 0.00001572425628773115},
+gender: "Man",
+race: {asian: 0.0007496751550206682,
+  black: 0.00015141853282329976,
+  indian: 0.017956600640900433,
+  'latino hispanic': 1.8069805577397346,
+  'middle eastern': 11.54509037733078,
+  white: 86.62906885147095},
+region: {x: 17, y: 16, w: 169, h: 169}}
+        ],
+        count: 1
+      }
     },
-  },
+  ],
   collage_image_string: 'base 64 string',
 };
 
