a7553fefda395d7258d065dede0c174c1ea3d9b4
Neilzon Viloria <neilcviloria@gmail.com>
2021-11-24 15:49:30 -0700

    clean up

--- api/database/__init__.py
+++ api/database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://ubuntu:ubuntu@2d7a6.yeg.rac.sh/postgres" # TODO: ipv6 psycopg2 docker build error
+# SQLALCHEMY_DATABASE_URL = "postgresql://ubuntu:ubuntu@2d7a6.yeg.rac.sh/postgres"
 SQLALCHEMY_DATABASE_URL = "postgresql://ohhykmmy:kC5gr49OfXLn1Kkufk_y1CtHJUAhTPwM@kashin.db.elephantsql.com/ohhykmmy"
 
 engine = create_engine(
